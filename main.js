b27ef11e26b29c70d034344c13920c89429826a4
taylorvercillo <taylornvcasey@gmail.com>
2022-01-04 22:46:44 -0700

    commit tree 5e5b3ceab10dd99b2ac9de45618f556be9f656bc

--- main.js
+++ main.js
@@ -3,7 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let heartsNodeArray = [...document.getElementsByClassName("like-glyph")] 
+let modal = document.getElementById('modal')
+let modalParagraph = document.getElementById('modal-message')
 
+let callServerAndCatch = (event) => {
+  mimicServerCall()
+  .then(() => handleResponse(event))
+  .catch(error => handleError(error))
+}
+
+let handleError= (errorMessage) => {
+  modal.classList.remove('hidden')
+  modalParagraph.innerText = errorMessage
+  setTimeout(() => {
+    modal.classList.add('hidden')
+    modalParagraph.innerText=''
+  }, 3000)
+}
+
+let handleResponse =(event) => {
+  if(event.target.textContent === EMPTY_HEART) {
+    event.target.classList.add('activated-heart')
+    event.target.textContent = FULL_HEART
+  } else {
+    event.target.classList.remove('activated-heart')
+      event.target.textContent = EMPTY_HEART
+  }
+}
+
+
+
+heartsNodeArray.map(heartNode => {
+  heartNode.addEventListener('click', callServerAndCatch); 
+})
 
 
 
